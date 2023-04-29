5e11d2ba7f11f833279ef8fa8f1068595a2bad78
ChernovMykola <4ernovnikola@gmail.com>
2023-04-29 22:54:20 +0200

    DeleteView

--- BookFace/myblog/views.py
+++ BookFace/myblog/views.py
@@ -21,7 +21,10 @@ from myblog.models import (
     UserProfileInfo, 
     User
 )
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
 from myblog.forms import (
     PostForm, 
     CommentForm, 
@@ -38,15 +41,7 @@ from django.http import (
 )
 from django.contrib.auth.models import User
 from django.db.utils import IntegrityError
-
-
-
-
-
-
 from django.urls import reverse_lazy
-
-
 
 
 class AboutView(TemplateView):
@@ -78,16 +73,7 @@ class AllPost(ListView):
             posts = Post.objects.filter(author=self.request.user)
         else:
             posts = Post.objects.none()
-
         return posts
-
-# def wall(request):
-#     if request.user.is_authenticated:
-#         posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
-#         context = {'posts': posts}
-#         return render(request, 'myblog/wall.html', context)
-#     else:
-#         return render(request, 'myblog/login.html')
 
 
 class Wall(ListView):
@@ -100,8 +86,6 @@ class Wall(ListView):
         if not self.request.user.is_authenticated:
             return redirect('accounts/login')
         return post
-    
-
 
 
 class PostDetail(DetailView):
@@ -109,7 +93,6 @@ class PostDetail(DetailView):
     template_name = 'myblog/post_detail.html'
     context_object_name = 'post'
     
-
 
 class CreatePostView(LoginRequiredMixin, CreateView):
     login_url = '/login/'
@@ -125,42 +108,21 @@ class CreatePostView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
     
-
-
-
-
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     login_url = '/login/'
     redirect_field_name = 'myblog/post_detail.html'
     model = Post
     form_class = PostForm
 
+  
+class DeletePostView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('myblog:post_list')
 
-@login_required
-def deletepost(request, pk):
-    if request.user.is_authenticated:
-        post = get_object_or_404(Post, pk=pk)
-        if post.author == request.user:
-            post.delete()
-        else:
-            return redirect('accounts/login')
-        return redirect('myblog:post_list')
-    else:
-        return redirect('accounts/login')
-
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
     
-    
-    
-
-
-# class DraftListView(LoginRequiredMixin, ListView):
-#     login_url = '/login/'
-#     redirect_field_name = 'myblog/post/list.html'
-#     model = Post
-
-
-    
-
     
 @login_required
 def post_publish(request, pk):
@@ -168,13 +130,14 @@ def post_publish(request, pk):
     post.publish()
     return redirect('myblog:post_detail', pk=pk )
 
+
 @login_required
 def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = form.save(commit=False)  # Change commit argument to True
+            comment = form.save(commit=False) 
             comment.post = post
             comment.author = request.user
             comment.save()
@@ -184,19 +147,6 @@ def add_comment_to_post(request, pk):
         form = CommentForm() 
     request.session['previous_page_url'] = request.META.get('HTTP_REFERER', '/')
     return render(request,'myblog/comment_form.html',{'form':form})
-    
-# @login_required
-# def comment_approve(request, pk):
-#     comment = get_object_or_404(Comment, pk=pk)
-#     comment.approve()
-#     return redirect('post_detail', pk=comment.post.pk)
-
-# @login_required
-# def comment_remove(request, pk):
-#     comment = get_object_or_404(Comment, pk=pk)
-#     post_pk = comment.post.pk
-#     comment.delete()
-#     return redirect('post_detail', pk=post_pk)
 
 
 def registration(request):
@@ -225,11 +175,7 @@ def registration(request):
             print(user_form.errors)
     else:
         user_form = UserForm()
-
     return render(request, 'myblog/registration.html', {'user_form': user_form, 'registered': registered})
-
-
-
 
 
 def user_login(request):
@@ -251,7 +197,5 @@ def user_login(request):
             print("Someone tried to login and failed!")
             print("Username: {} and password: {}".format(username, password))
             return render(request, 'myblog/user_login.html',{})
-            
-        # HttpResponse("Invalid login details! supplied!")
     else:
         return render(request, 'myblog/user_login.html',{})
