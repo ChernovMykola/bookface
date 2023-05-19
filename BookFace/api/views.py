d4ef3f2aead16c5ff7f47aabd9020b20e98d307b
ChernovMykola <4ernovnikola@gmail.com>
2023-05-19 21:26:05 +0200

    PostListApiView

--- BookFace/api/views.py
+++ BookFace/api/views.py
@@ -3,5 +3,18 @@ from .serializers import (
     PostSerializer,
     CommentSerializer
 )
+from rest_framework import (
+    generics,
+    permissions,
+)
+from myblog.models import (
+    Post,
+    Comment
+)
 
-# Create your views here.
+class PostList(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+ 
+    def get_queryset(self):
+        return Post.objects
