fd70804c3d7133a32474ec69d243c8b9fe21546a
ChernovMykola <4ernovnikola@gmail.com>
2023-05-22 17:17:47 +0200

    logApi

--- BookFace/api/views.py
+++ BookFace/api/views.py
@@ -3,6 +3,7 @@ from .serializers import (
     PostSerializer,
     CommentSerializer
 )
+from django.contrib.auth import authenticate
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http import JsonResponse
@@ -29,6 +30,21 @@ def signup(request):
         except IntegrityError:
             return JsonResponse({'error':'That username has already been taken. Please choose a new username'},
                                 status=400)
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(request, username=data['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error': 'Please check email or password!'},
+                                status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=200)
 
 
 class PostList(generics.ListAPIView):
